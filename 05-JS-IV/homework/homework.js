77af762aa364985101465bf1e54ce732d0329c02
Your Name <you@example.com>
2023-01-24 13:28:18 -0500

    HW 05-JS-IV terminado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -21,6 +21,7 @@ function agregarPropiedad (objeto, property) {
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
   objeto[property] = null
+  
   return objeto
 }
 
